5fa5c9916b846a77dc35b04533cc1ebddf1c2da5
mazhihua <mazhihua@bftv.com>
2017-11-02 18:49:32 +0800

    学习layout直接引用扩展插件使用，for循环、类对象使用

---
+++ app/src/main/java/com/kn/kotlin/activity/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.kn.kotlin.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.kn.kotlin.R
+import com.kn.kotlin.entity.Student
+import kotlinx.android.synthetic.main.main_activity.*
+
+/**
+ * Created by MaZhihua on 2017/11/2.
+ * Kotlin学习主页面
+ */
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+    val TAG:String = MainActivity::class.java.simpleName
+
+    var string_array:Array<String> = arrayOf("for循环","类对象Obj")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        initView()
+    }
+
+    fun initView(){
+        if(string_array!!.size != 0){
+            for(i in string_array.indices){
+                var text = string_array[i];
+                val btn = Button(this)
+                btn.text = text;
+                btn.setOnClickListener(this)
+                btn.id = i
+                btnContainer.addView(btn)
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        val id = v?.id
+        when(id){
+            0 -> forRecycle()
+            1 -> kotlinObj()
+        }
+    }
+
+    /**
+     * for循环
+     */
+    fun forRecycle(){
+
+        // type 1
+        for(i in 5 until 10){
+            Log.d(TAG,"左闭右开循环，包括5，不包括10 ,i = $i")
+        }
+
+        // type 2
+        for(i in 10..20 step 2){
+            Log.d(TAG,"递增循环，每次递增2 ,i = $i")
+        }
+
+        // type 3
+        for(i in 10 downTo 5){
+            Log.d(TAG,"递减循环，每次递减5 ,i = $i")
+        }
+
+    }
+
+    /**
+     * 类对象s
+     */
+    fun kotlinObj(){
+        val student = Student("KNothing","哈佛",1.80F,100)
+        Log.d(TAG,"obj msg = $student")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/kn/kotlin/entity/Student.kt
@@ -0,0 +1,6 @@
+package com.kn.kotlin.entity
+
+/**
+ * Created by MaZhihua on 2017/11/2.
+ */
+data class Student(val name:String, val schoolName:String,val height:Float, val age:Int)
