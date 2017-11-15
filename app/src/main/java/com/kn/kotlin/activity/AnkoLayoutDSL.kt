87c34bcae3d9918b512dc1985c2d2a753ad3c72c
mazhihua <mazhihua@bftv.com>
2017-11-15 18:56:15 +0800

    报错处理

--- app/src/main/java/com/kn/kotlin/activity/AnkoLayoutDSL.kt
+++ app/src/main/java/com/kn/kotlin/activity/AnkoLayoutDSL.kt
@@ -4,6 +4,7 @@ import android.view.Gravity
 import android.view.View
 import android.widget.LinearLayout
 import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 /**
  * Created by MaZhihua on 2017/11/6.
@@ -23,28 +24,18 @@ class AnkoLayoutDSL : AnkoComponent<AnkoLayoutActivity> {
 
 
                 var editText = editText {
-                    lparams {
-                        width = matchParent
-                        height = dip(50)
-                        topMargin = dip(16)
-
-                    }
+                    width = matchParent
+                    height = dip(50)
                 }
 
                 var editText2 = editText {
-                    lparams {
-                        width = matchParent
-                        height = dip(50)
-                        topMargin = dip(16)
-                    }
+                    width = matchParent
+                    height = dip(50)
                 }
 
                 var loginBtn = button("Fucking") {
-                    lparams {
-                        width = matchParent
-                        height = dip(60)
-                        topMargin = dip(16)
-                    }
+                    width = matchParent
+                    height = dip(60)
 
                     onClick {
                         toast("点击了按钮")
